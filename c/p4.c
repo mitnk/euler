c4f0f492db315bb239ec8b5ca34fdc682d3736b4
mitnk <whgking@gmail.com>
2011-11-08 17:46:17 +0800

    C: added problem 4.

---
+++ c/p4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+main()
+{
+    char buf[31], result[31];
+    int max = 0;
+    int n = 0;
+    int i, j;
+    for (i = 100; i < 1000; i++)
+    {
+        for (j=100; j < 1000; j++)
+        {
+            sprintf(buf, "%d", i * j);
+            if (pali(buf) && max < i * j)
+                max = i * j;
+        }
+            
+    }
+    printf("%d\n", max);
+    return 0;
+}
+
+int pali(char * s)
+{
+    int len = strlen(s);
+    if (len % 2 != 0)
+        return 0;
+
+    int i;
+    for (i = 0; i < len / 2; i++)
+    {
+        if (s[i] != s[len - i - 1])
+            return 0;
+    }
+    return 1;
+}
