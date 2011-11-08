cdda82eb5fae4c35189ff8aaa33a5f876d5bfba4
mitnk <whgking@gmail.com>
2011-11-08 19:33:13 +0800

    C: added problem 7.

---
+++ c/p7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+main()
+{
+    int n = 1;
+    int i;
+    for (i = 3; ; i += 2)
+    {
+        if (is_prime(i))
+        {
+            ++n;
+            if (n == 10001)
+                break;
+        }
+    }
+    printf("%d\n", i);
+    return 0;
+}
+
+int is_prime(unsigned long long n)
+{
+    if (n < 2)
+        return 0;
+    if (n == 2)
+        return 1;
+    if (n % 2 == 0)
+        return 0;
+
+    unsigned long long i = 0;
+    for (i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return 0;
+    return 1;
+}
