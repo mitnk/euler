1ceb1970bcdab32aa25ae41d5d2dc51ea05534ff
mitnk <whgking@gmail.com>
2011-11-08 18:41:51 +0800

    C: added problem 5.

---
+++ c/p5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+main()
+{
+    unsigned long long i;
+    unsigned long long step = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19;
+    for (i = step; ; i += step)
+    {
+        if (i % 4 == 0 && \
+            i % 6 == 0 && \
+            i % 8 == 0 && \
+            i % 9 == 0 && \
+            i % 10 == 0 && \
+            i % 12 == 0 && \
+            i % 14 == 0 && \
+            i % 15 == 0 && \
+            i % 16 == 0 && \
+            i % 18 == 0 && \
+            i % 20 == 0)
+            break;
+    }
+    printf("%lld\n", i);
+}
