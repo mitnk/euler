14ee5f730331b41412cc3ae0b06d862876a79d42
mitnk <whgking@gmail.com>
2012-01-30 23:42:07 +0800

    add erlang p1 another solution.

--- erlang/p001.erl
+++ erlang/p001.erl
@@ -2,7 +2,7 @@
 % Find the sum of all the multiples of 3 or 5 below 1000.
 % 
 -module(p001).
--export([solution/0, sum/1]).
+-export([solution/0, sum/1, solution2/0, mult3or5/1]).
 
 solution() ->
     sum(999).
@@ -17,4 +17,18 @@ sum(N) ->
             N + sum(N - 1);
         true ->
             sum(N - 1)
+    end.
+
+
+solution2() ->
+    N = 999,
+    lists:sum([X || X <- lists:seq(1, N), mult3or5(X) ]).
+
+
+mult3or5(X) ->
+    if
+        X rem 3 ==0; X rem 5 == 0 ->
+            true;
+        true ->
+            false
     end.
