98a1c4b31e973fb72e451d9857fd7700077f4e30
mitnk <whgking@gmail.com>
2012-01-30 23:29:18 +0800

    update erlang p1.

--- erlang/p001.erl
+++ erlang/p001.erl
@@ -4,10 +4,12 @@
 solution() ->
     sum(999).
 
-sum(3) ->
-    3;
 sum(N) ->
     if
+        N < 3 ->
+            0;
+        N == 3 ->
+            3;
         N rem 3 == 0; N rem 5 == 0 ->
             N + sum(N - 1);
         true ->
