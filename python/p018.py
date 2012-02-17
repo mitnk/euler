6cb3aed78c859b19e3bfea12415bb05c5f42d7c8
mitnk <whgking@gmail.com>
2012-02-17 14:46:46 +0800

    update duplicate programs, made it more reuseable.

--- python/p018.py
+++ python/p018.py
@@ -1,19 +1,22 @@
-f = open('p018.data')
-L = []
-for line in f:
-    L.append([int(x) for x in line.split(' ')])
+def max_sum_of_triangle(file_name):
+    def sum(L):
+        if len(L) == 1:
+            return L[0][0]
+        else:
+            last = L[-1]
+            L.remove(last)
+            for i in range(len(L[-1])):
+                if last[i] >= last[i + 1]:
+                    L[-1][i] += last[i]
+                else:
+                    L[-1][i] += last[i + 1]
+            return sum(L)
 
-def sum(L):
-    if len(L) == 1:
-        return L[0][0]
-    else:
-        last = L[-1]
-        L.remove(last)
-        for i in range(len(L[-1])):
-            if last[i] >= last[i + 1]:
-                L[-1][i] += last[i]
-            else:
-                L[-1][i] += last[i + 1]
-        return sum(L)
+    f = open(file_name)
+    L = []
+    for line in f:
+        L.append([int(x) for x in line.split(' ')])
+    return sum(L)                
 
-print sum(L)
+if __name__ == "__main__":
+    print max_sum_of_triangle('p018.data')
