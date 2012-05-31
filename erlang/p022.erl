690416bf4e64baf9235f8d8ef68e9f2907a9c3bf
mitnk <whgking@gmail.com>
2012-05-31 14:54:11 +0800

    code review.

--- erlang/p022.erl
+++ erlang/p022.erl
@@ -19,8 +19,9 @@ sum(Index, N, L) ->
 read_to__sorted_list() ->
     {ok, S} = file:open("p022.txt", read),
     Content = io:get_line(S, ''),
+    file:close(S),
     L = [string:strip(X, both, $") || X <- string:tokens(Content, ",")],
-    lists:usort(L).
+    lists:sort(L).
 
 score(I, S) ->
     N = lists:sum([X - $A + 1 || X <- S]),
